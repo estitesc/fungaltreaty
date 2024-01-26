34b4944676e858f995a9603509ab7d6813468432
Evan Stites-Clayton <stitesclayton@gmail.com>
2024-01-26 10:15:48 -0800

    backlin kupdate

--- src/app/loodorf/page.tsx
+++ src/app/loodorf/page.tsx
@@ -5,7 +5,7 @@ export default function Remembrant() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Link href="purger">
+        <Link href="/">
           <Figmint>dance they dunce</Figmint>
         </Link>
       </div>
